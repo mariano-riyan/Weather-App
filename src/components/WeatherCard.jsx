a9e0299c1cc0677da8a4306b322282e030175330
aisatmariano224488 <aisat.mariano224488@gmail.com>
2026-06-30 23:07:35 +0800

    feat: enhance WeatherCard and Card components for better data handling and layout

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -15,26 +15,34 @@ function WeatherCard({ weather }) {
     return ( 
         <div className="my-4 space-y-16">
             <div className="min-h-[50vh] grid place-items-center md:justify-start">
-                <h1 className="text-3xl font-bold">{name}, {country}</h1>
+                <h1 className="text-3xl font-bold text-center">{name || ''}, {country || ''}</h1>
 
                 <div className="grid place-items-center md:flex md:gap-8">
                     <img 
-                        src={iconUrl}
-                        alt={description}
+                        src={iconUrl || ''}
+                        alt={description || ''}
                         className=""
                     />
 
                     <div className="text-center space-y-1 md:text-start">
-                        <p className="text-7xl">{temp.toFixed()}°C</p>
-                        <p className="text-sm">Feels like {feels_like.toFixed()}°C</p>
-                        <p className="text-lg capitalize">{description}</p>
+                        <p className="text-7xl">{temp.toFixed() || ''}°C</p>
+                        <p className="text-sm">Feels like {feels_like.toFixed() || ''}°C</p>
+                        <p className="text-lg capitalize">{description || ''}</p>
                     </div>
                 </div>
             </div>
 
-            <div className="grid grid-cols-2 gap-4 md:flex">
-                <Card name="HUMIDITY" value={humidity} unit='%' />
-                <Card name="WIND SPEED" value={(speed * 3.6).toFixed(1)} unit='km/h' />
+            <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+                <Card 
+                    name="HUMIDITY" 
+                    value={humidity || ''} 
+                    unit='%'
+                />
+                <Card 
+                    name="WIND SPEED" 
+                    value={(speed * 3.6).toFixed(1) || ''} 
+                    unit='km/h' 
+                />
             </div>
         </div>
     );
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,6 +1,7 @@
 function Card({ name, value, unit }) {
+
     return ( 
-        <div className="p-4 bg-foreground/10 backdrop-blur-xl rounded-xl border border-white/20 space-y-8 md:min-w-50">
+        <div className="p-4 md:p-6 xl:p-8 bg-foreground/10 backdrop-blur-xl rounded-xl border border-white/20 md:min-w-50">
             <h6 className="text-xs">{name}</h6>
             <span className="text-4xl font-bold">
                 {value}
