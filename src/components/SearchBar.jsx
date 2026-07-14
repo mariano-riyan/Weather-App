b83a0177d823624f8abf3d3906a121d1973ebb26
mariano-riyan <marianoriyan@gmail.com>
2026-07-14 18:53:02 +0800

    feat: implement SearchBar component with new Input and ButtonGroup UI elements

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
-import { Search, X } from "lucide-react";
+import { Search } from "lucide-react";
 
 import SearchHistory from "./SearchHistory";
+import { Input } from "./ui/input";
+import { ButtonGroup } from "./ui/button-group";
+import { Button } from "./ui/button"
 
-function SearchBar({ value, onCityChange, onSearch, history, onXClick }) {
+function SearchBar({ value, onCityChange, onSearch, history }) {
 
     const [showX, setShowX] = useState('opacity-0');
 
@@ -22,16 +25,16 @@ function SearchBar({ value, onCityChange, onSearch, history, onXClick }) {
     }
     
     return ( 
-        <div>
-            <form 
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    onSearch();
-                }}
-            >
-                <div className="rounded-full bg-foreground/50 p-2 flex place-self-center md:place-self-start group">
-                    <Search className="inline-block mx-4" />
-                    <input 
+        <form 
+            onSubmit={(e) => {
+                e.preventDefault();
+                onSearch();
+            }}
+            className="place-items-center"
+        >
+            <div>
+                <ButtonGroup>
+                    <Input 
                         type="text"
                         placeholder="Search City..."
                         aria-label="City"
@@ -41,24 +44,22 @@ function SearchBar({ value, onCityChange, onSearch, history, onXClick }) {
                         onFocus={() => {setShowX('opacity-100 cursor-pointer')}}
                         onBlur={() => {setShowX('opacity-0 cursor-default')}}
                         onKeyDown={handleEnter}
-                        className="outline-none w-full"
+                        className="p-4 md:text-md lg:text-lg xl:text-xl"
                     />
-                    
-                    <button
-                        type="button"
-                        aria-label="Clear City"
-                        className={`${showX} bg-transparent border-0 p-0`}
-                        onClick={() => {
-                            onXClick();
-                            setShowX('opacity-0 cursor-default')
-                        }}
+
+                    <Button
+                        type="submit"
+                        className="p-4"
                     >
-                        <X />
-                    </button>
-                </div>
+                        <Search />
+                    </Button>
+                </ButtonGroup>
+
                 <SearchHistory history={history} onCityClick={handleHistoryClick} visibility={showX}/>
-            </form>
-        </div>
+            </div>
+
+
+        </form>
     );
 }
 
