e390e1e6b05bab4e2222f9b73e2d2168dc70f965
mariano-riyan <marianoriyan@gmail.com>
2026-07-23 17:36:50 +0800

    fix: Adjust layout of WeatherContents for better responsiveness

--- src/components/layout/WeatherContents.jsx
+++ src/components/layout/WeatherContents.jsx
@@ -17,13 +17,13 @@ const WeatherContents = () => {
     return (
 			( isPending || forecastData && weatherData) && (
 				<div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
-                    <div className="flex flex-col col-span-2">
+                    <div className="flex flex-col col-span-1 lg:col-span-2">
                         <WeatherCard weather={weatherData} tempUnit={unit} onLoading={isLoading} />
                         <ForecastList forecasts={forecastData?.list} onLoading={isLoading}/>
                         <HourlyForecastList forecasts={forecastData?.list} onLoading={isLoading} />
                     </div>
-                    <div className="">
-                        <WeatherElements weather={weatherData} tempUnit={unit} onLoading={isLoading} />
+                    <div>
+                        <WeatherElements weather={weatherData} unit={unit} onLoading={isLoading} />
                     </div>
                 </div>
             )
